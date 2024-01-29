e01497ab8607706283f49fa34d44d3082b7d2c95
nagvanshi9275 <106686974+nagvanshi9275@users.noreply.github.com>
2024-01-29 12:44:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chandragupta
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nagvanshi9275/chandragupta)
---
+++ src/Main.js
@@ -0,0 +1,97 @@
+
+
+
+import React from "react";
+
+import {TextField} from "@mui/material"
+
+
+export default function Main(){
+
+
+     return(
+       <>
+
+       <div className="Baap">
+
+
+           <div className="input">
+
+<TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                name="password"
+              />
+
+      </div>
+
+
+
+
+       
+      <div  className="input">
+
+<TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                name="password"
+              />
+
+      </div>
+
+
+
+
+
+
+           
+      <div className="input">
+
+<TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                name="password"
+              />
+
+      </div>
+
+         
+
+
+
+
+
+
+
+
+       </div>
+
+
+      </>
+
+
+     )
+
+
+
+}
+
+
+
+
+
+
+
+
